7a6dcec82b1f63c300dcd30f650d55ec89699969
Adriana <dri19tcc@gmail.com>
2016-05-22 17:50:22 -0700

    Added food model testing

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -4,34 +4,27 @@ class UserTest < ActiveSupport::TestCase
   def setup
     # @known = OmniAuth.config.mock_auth[:spotify_known]
     @known = users(:known_user)
-    @auth_hash = {"info" => {"name" => "Adriana Cannon"}, "provider" => "spotify", "uid" => "dri19tcc"}
-    # @unknown = users(:unknown_user["info"])
-    # @unknown_with_uid = OmniAuth.config.mock_auth[:spotify_uid]
+    @unknown = users(:unknown_user)
   end
 
   test "can find an existing user given an oauth spotify hash" do
+    @auth_hash = {"info" => {"name" => "Adriana Cannon"}, "provider" => "spotify", "uid" => "dri19tcc"}
     assert_equal @known, User.find_or_create_from_omniauth(@auth_hash)
   end
 
   test "can make a new user given the oauth spotify hash of an unknown user" do
-    # puts "User count first is #{User.count}"
-    puts @unknown.inspect
+    @auth_hash = {"info" => {"name" => "Chris Cannon"}, "provider" => "spotify", "uid" => "cannonc"}
     assert_difference 'User.count', 1 do
-      @user = User.find_or_create_from_omniauth(@unknown)
-      # puts "User count after block is #{User.count}"
+      @user = User.find_or_create_from_omniauth(@auth_hash)
     end
   end
 
-  # test "uses oauth data to set user name, provider and uid for new users" do
-  #   user = User.find_or_create_from_omniauth @unknown
-  #
-  #   assert_equal @unknown['name'], user.name
-  #   assert_equal @unknown['provider'], user.provider
-  #   assert_equal @unknown['uid'], user.uid
-  # end
-  #
-  # test "prefers a top-level uid over a nested id when creating an oauth user" do
-  #   user = User.find_or_create_from_omniauth @unknown_with_uid
-  #   assert_equal @unknown_with_uid['uid'], user.uid
-  # end
+  test "uses oauth data to set user name, provider and uid for new users" do
+    @auth_hash = {"info" => {"name" => "Suzanne Convertino Harrison"}, "provider" => "spotify", "uid" => "1226771573"}
+    user = User.find_or_create_from_omniauth(@auth_hash)
+
+    assert_equal @unknown['name'], user.name
+    assert_equal @unknown['provider'], user.provider
+    assert_equal @unknown['uid'], user.uid
+  end
 end
